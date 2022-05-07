ea33a91b346bcf85cd6f82ca779ec745524cd111
mkflow27 <mkflow27@web.de>
2022-05-07 08:41:01 +0200

    added script

--- scripts/generate-file.js
+++ scripts/generate-file.js
@@ -6,6 +6,6 @@ var rnd = Math.random()
 var str = rnd.toString()
 console.log(str)
 
-//fs.writeFileSync(`${str}.txt`, Date(Date.now()))
-fs.writeFileSync("sample.txt", Date(Date.now()))
+fs.writeFileSync(`${str}.txt`, Date(Date.now()))
+//fs.writeFileSync("sample.txt", Date(Date.now()))
 
